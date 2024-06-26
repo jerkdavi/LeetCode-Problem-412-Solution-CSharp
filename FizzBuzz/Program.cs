4583fdd831dd9116b1c428d08ba2fad665b7c088
David Jerković <jerkdavi@gmail.com>
2024-06-26 19:18:25 +0200

    Commit of LeetCode Problem 412. Solution

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //412.
+            //int n = 3;
+            //int n = 5;
+            //int n = 15;
+            Console.Write("Enter value of number n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            IList<string> list = Solution.FizzBuzz(n);
+            
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i != 0)
+                {
+                    Console.Write(",");
+                }
+                Console.Write(list[i]);
+            }
+
+            Console.WriteLine("\nDone processing");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FizzBuzz/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class Solution
+    {
+        public static IList<string> FizzBuzz(int n)
+        {
+            List<string> output = new List<string>();
+            string word;
+
+            for (int i = 0; i < n; i++)
+            {
+                word = "";
+                if ((i + 1) % 3 == 0)
+                {
+                    word += "Fizz";
+                }
+                if ((i + 1) % 5 == 0)
+                {
+                    word += "Buzz";
+                }
+                if(word == "")
+                {
+                    word += $"{i + 1}";
+                }
+                output.Add(word);
+            }
+            return output;
+        }
+    }
+}
